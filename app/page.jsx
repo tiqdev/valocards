fd4b498b80db98d6ca35de040a3dc7893b733f71
Tarık KAYA <tiqqq24@gmail.com>
2024-03-29 02:49:52 +0300

    Update gap between elements in main container

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import CardPreview from "@/components/preview-card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-svh w-full flex-col items-center justify-center gap-12">
+    <main className="flex min-h-svh w-full flex-col items-center justify-center md:gap-12 gap-4">
       <CardPreview />
       <SheetForm />
     </main>
