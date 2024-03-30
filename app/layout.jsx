67430cfda6b650356d78b878c88044915e2fa73e
Tarık KAYA <tiqqq24@gmail.com>
2024-03-31 01:15:34 +0300

    Add meta tags for SEO and social media sharing

--- app/layout.jsx
+++ app/layout.jsx
@@ -38,7 +38,45 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Provider store={store}>
         <head>
-          <title>Hello world</title>
+          <meta name="theme-color" content={"#291911"} id="theme-color-meta" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+          <title>ValoCards | tiqdev</title>
+          <meta name="title" content="ValoCards | tiqdev" />
+          <meta
+            name="description"
+            content="Valorant Player Card Generator is a tool to create custom player cards for Valorant players."
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://valocards.vercel.app/" />
+          <meta property="og:title" content="ValoCards | tiqdev" />
+          <meta
+            property="og:description"
+            content="Valorant Player Card Generator is a tool to create custom player cards for Valorant players."
+          />
+          <meta
+            property="og:image"
+            content="https://valocards.vercel.app/valocards_og.png"
+          />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://valocards.vercel.app/"
+          />
+          <meta property="twitter:title" content="ValoCards | tiqdev" />
+          <meta
+            property="twitter:description"
+            content="Valorant Player Card Generator is a tool to create custom player cards for Valorant players."
+          />
+          <meta
+            property="twitter:image"
+            content="https://valocards.vercel.app/valocards_og.png"
+          />
+
+          <meta name="robots" content="index, follow" />
+          <meta name="googlebot" content="index, follow" />
           <meta
             property="og:image"
             content="https://valocards.vercel.app/valocards_og.png"
